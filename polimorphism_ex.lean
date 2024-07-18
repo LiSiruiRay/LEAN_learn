84397d3f88dfc2947c6915ff08336d6d909e1de2
Sirui Li <slray@nyu.edu>
2024-07-18 16:06:15 +0800

    exercise of polymorphism

--- polimorphism_ex.lean
+++ polimorphism_ex.lean
@@ -39,3 +39,43 @@ def test_prod : Prod String (Prod Nat Int) := ("123", 45, -5)
 -- inductive PetName (α : Type) : Type where
 --   | Dog : α → String
 --   | Cat : α → String
+
+/-
+
+Write a function zip that combines two lists into a list of pairs. The resulting list should be as long as the shortest input list. Start the definition with `def zip {α β : Type} (xs : List α) (ys : List β) : List (α × β) :=`.
+
+-/
+
+def zip {α β : Type} (xs : List α) (ys : List β) : List (α × β) :=
+  match xs with
+    | [] => List.nil
+    | a :: b =>
+      match ys with
+        | [] => List.nil
+        | c :: d => List.cons (Prod.mk a c) (zip b d)
+
+def list_one : List String := ["a", "b", "c", "d"]
+def list_two : List Int := [-1, 2, 3]
+
+#eval zip list_one list_two
+#eval zip list_two list_one
+
+
+/-
+Write a polymorphic function `take` that returns the first $n$
+ entries in a list, where n
+ is a Nat. If the list contains fewer than n entries, then the resulting list should be the input list. #eval take 3 ["bolete", "oyster"] should yield ["bolete", "oyster"], and #eval take 1 ["bolete", "oyster"] should yield ["bolete"].
+-/
+
+def take {α : Type} (n : Nat) (l : List α) : List α :=
+  match n with
+    | 0 => List.nil
+    | Nat.succ k =>
+      match l with
+        | [] => List.nil
+        | a :: b => List.cons (a) (take k b)
+
+#eval take 3 ["bolete", "oyster"]
+#eval take 1 ["bolete", "oyster"]
+-- #eval List.cons ("List.nil") (List.cons ("List.nil") (List.nil))
+-- TODO: so nil → nil has no effect?
