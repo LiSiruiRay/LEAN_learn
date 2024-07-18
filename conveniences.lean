b7c9d0f9e2dfd2a304107c388852b39a4785e070
Sirui Li <slray@nyu.edu>
2024-07-18 21:12:30 +0800

    conveniences-helper

--- conveniences.lean
+++ conveniences.lean
@@ -21,3 +21,36 @@ def drop : Nat → List α → List α
 def test_list : List Nat := [1, 2, 3, 4, 5]
 
 #eval drop 4 test_list
+
+
+def fromOption (defualt : α) : Option α → α
+  | none => defualt
+  | some x => x
+
+def unzip : List (α × β) → List α × List β
+  | [] => ([], [])
+  | (x, y) :: xyz => (x :: (unzip xyz).fst, y :: (unzip xyz).snd)
+
+
+
+-- def unzip_faster : List (α × β) → List α × List β
+--   | [] => ([], [])
+--   | (x, y) :: xyz
+--   let unzipped : List α × List β := unzip_faster xyz
+--   => x :: unzipped.fst × y :: unzipped.snd
+
+def unzip_faster : List (α × β) → List α × List β
+  | [] => ([], [])
+  | (x, y) :: xyz =>
+    let unzipped : List α × List β := unzip_faster xyz
+    (x :: unzipped.fst, y :: unzipped.snd)
+
+
+def reverse (xs : List α) : List α :=
+  let rec helper : List α → List α → List α
+      | [], sofar => sofar
+      | a :: as, sofar => helper as (a :: sofar)
+    helper xs []
+
+def test_list : List Int := [1, 2, 3, 4, 5]
+#eval reverse test_list
